49e750c76a8f1187554c0b56c46212902de42312
Andraz Sraka <a@aufbix.org>
2019-10-01 15:34:43 +0200

    - Update to use OpenJRE 11 - Enable Antialiased fonts (issue on linux)

--- _data/run.sh
+++ _data/run.sh
@@ -5,8 +5,8 @@ echo "[+] JVM Mem: ${ENV_MEM_JVM}"
 
 if [ "$ENV_BURP_PRO" -eq "1" ]; then
     echo "[+] Running Burp Pro"
-    java -jar -Xmx${ENV_MEM_JVM}  /home/tester/sources/burp_pro.jar
+    java -jar -Xmx${ENV_MEM_JVM} -Dawt.useSystemAAFontSettings=on /home/tester/sources/burp_pro.jar
 else
     echo "[+] Running Burp Free"
-    java -jar -Xmx${ENV_MEM_JVM}  /home/tester/sources/burp_free.jar
+    java -jar -Xmx${ENV_MEM_JVM} -Dawt.useSystemAAFontSettings=on /home/tester/sources/burp_free.jar
 fi
